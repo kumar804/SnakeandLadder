ae910dbe45b519cc90a8dfca00dfa4eb0246a268
kumar804 <kumarsinghmanish547@gmail.com>
2021-07-14 07:49:49 +0530

    added condition

--- Snakeandladder/SnakeAndLadder.java
+++ Snakeandladder/SnakeAndLadder.java
@@ -9,6 +9,13 @@ public class SnakeAndLadder {
 		int position = 0;
 		System.out.println("The player Position is:-" + position);
 		
+		while (position <= 100) {
+			if (position < 0)
+			{
+				position = 0;
+			}
+			
+		
 		//added dice roll UC2
 		int diceRoll = (int) (Math.floor(Math.random() * 10 ) % 6 + 1);
 		System.out.println("Dice Roll: " + diceRoll);
@@ -17,6 +24,25 @@ public class SnakeAndLadder {
 		
 		//random check
 		int option = (int) (Math.floor(Math.random() * 10 ) % 3 + 1);
+		switch (option) {
+		
+		case 1:
+			System.out.println("No Play");
+			System.out.println("Position: " + position);
+			break;
+		case 2:
+			System.out.println("Ladder");
+			position = position + diceRoll;
+			System.out.println("Position: " + position);
+			break;
+		case 3:
+			System.out.println("Snake");
+			position = position - diceRoll;
+			System.out.println("Position: " + position);
+			break;
+		
+		}
 	}
 
+}
 }
