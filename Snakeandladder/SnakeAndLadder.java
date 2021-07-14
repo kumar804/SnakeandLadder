0e904f23f37aec89083ed23ad5243091362cf3a2
kumar804 <kumarsinghmanish547@gmail.com>
2021-07-14 07:12:08 +0530

    added dice roll

--- Snakeandladder/SnakeAndLadder.java
+++ Snakeandladder/SnakeAndLadder.java
@@ -8,6 +8,12 @@ public class SnakeAndLadder {
 		//Printing player position UC1
 		int position = 0;
 		System.out.println("The player Position is:-" + position);
+		
+		//added dice roll UC2
+		int diceRoll = (int) (Math.floor(Math.random() * 10 ) % 6 + 1);
+		System.out.println("Dice Roll: " + diceRoll);
+		position = position + diceRoll;
+		System.out.println("New Position of Player is: " + position);
 	}
 
 }
