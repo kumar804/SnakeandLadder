f4069f4d72f262098559fb09276a615da8bc8822
kumar804 <kumarsinghmanish547@gmail.com>
2021-07-14 07:59:09 +0530

    added condition to roll dice till final value 100 achieved

--- Snakeandladder/SnakeAndLadder.java
+++ Snakeandladder/SnakeAndLadder.java
@@ -7,9 +7,11 @@ public class SnakeAndLadder {
 		
 		//Printing player position UC1
 		int position = 0;
+		int diceRoll = 0;
 		System.out.println("The player Position is:-" + position);
 		
 		while (position <= 100) {
+			int win = 100;
 			if (position < 0)
 			{
 				position = 0;
@@ -17,8 +19,8 @@ public class SnakeAndLadder {
 			
 		
 		//added dice roll UC2
-		int diceRoll = (int) (Math.floor(Math.random() * 10 ) % 6 + 1);
-		System.out.println("Dice Roll: " + diceRoll);
+		 diceRoll = (int) (Math.floor(Math.random() * 10 ) % 6 + 1);
+		System.out.println("DICE ROLLED AND YOU HAVE: " + diceRoll);
 		position = position + diceRoll;
 		System.out.println("New Position of Player is: " + position);
 		
@@ -27,22 +29,30 @@ public class SnakeAndLadder {
 		switch (option) {
 		
 		case 1:
-			System.out.println("No Play");
-			System.out.println("Position: " + position);
+			System.out.println("PLAYER HAS PASSED HIS CHANCE");
+			System.out.println("POSITION AFTER PASS: " + position);
 			break;
 		case 2:
-			System.out.println("Ladder");
-			position = position + diceRoll;
-			System.out.println("Position: " + position);
-			break;
+			System.out.println("PLAYER GOT A LADDER!!");
+			if ((position + diceRoll) > win)
+			{
+				position = position - diceRoll;
+				break;
+			}
+			else
+			{
+				position = position + diceRoll;
+				System.out.println("POSITION AFTER LADDER: " + position);
+				break;
+			}
 		case 3:
-			System.out.println("Snake");
+			System.out.println("PLAYER GOT A SNAKE");
 			position = position - diceRoll;
-			System.out.println("Position: " + position);
+			System.out.println("POSITION AFTER SNAKE: " + position);
 			break;
 		
 		}
 	}
 
 }
-}
+	}
