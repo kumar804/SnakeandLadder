ffbf2cbfd690dd8259d016595cc6b42ccca943cf
kumar804 <kumarsinghmanish547@gmail.com>
2021-07-14 07:07:24 +0530

    printing player position

--- Snakeandladder/SnakeAndLadder.java
+++ Snakeandladder/SnakeAndLadder.java
@@ -4,6 +4,10 @@ public class SnakeAndLadder {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to SnakeAndLadder");
+		
+		//Printing player position UC1
+		int position = 0;
+		System.out.println("The player Position is:-" + position);
 	}
 
 }
